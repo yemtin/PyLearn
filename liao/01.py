47e44d92af56cd16be221cc3f5aba2e4b9fa9afb
yemtin <yemtin@qq.com>
2022-11-08 22:32:27 +0800

    fifth

--- liao/01.py
+++ liao/01.py
@@ -1,1 +1,5 @@
-print("\n".join(["".join([('love'[(x-y)%len('love')]if((x*0.05)**2+(y*0.1)**2-1)**3-(x*0.05)**2*(y*0.1)**3<=0 else' ')for x in range (-30,30)])for y in range(15,-15,-1)]))
+import time
+words = input('Lee,happy one-year anniversary!:')
+for item in words.split():
+    print('\n'.join([''.join([(item[(x-y) % len(item)] if ((x*0.05)**2+(y*0.1)**2-1)**3-(x*0.05)**2*(y*0.1)**3 <= 0 else ' ') for x in range(-30, 30)]) for y in range(12, -12, -1)]))
+    time.sleep(1.5)
