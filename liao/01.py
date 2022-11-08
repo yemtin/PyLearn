17c4045ab368b15fe3cbe688d7dfbc9c7485b5bf
yemtin <yemtin@qq.com>
2022-11-08 09:16:56 +0800

    fourth

--- liao/01.py
+++ liao/01.py
@@ -1,6 +1,1 @@
-print("hello world")
-print(100+200)
-print("100+200=",100+200)
-
-name="yemtin"
-print("hello",name)
+print("\n".join(["".join([('love'[(x-y)%len('love')]if((x*0.05)**2+(y*0.1)**2-1)**3-(x*0.05)**2*(y*0.1)**3<=0 else' ')for x in range (-30,30)])for y in range(15,-15,-1)]))
