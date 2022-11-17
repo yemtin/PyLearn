65e80bddb58ac219c9e335cb20cc33dd05bc7d78
yemtin <yemtin@qq.com>
2022-11-18 07:45:27 +0800

    thirteenth

--- liao/01.py
+++ liao/01.py
@@ -14,4 +14,12 @@ print("hello,lbc")
 print("love you")
 print("i wanna be better")
 print("ohoh")
+print(0b0101111)
+print(0o176)
+print(0x1EAF)
+a=3.14159
+print(a,type(a))
 
+n1=1.1
+n2=2.2
+print(n1=n2)
