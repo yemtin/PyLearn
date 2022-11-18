085417eb7f9001b8465c2ca26a5c7f62a50a6e7d
yemtin <yemtin@qq.com>
2022-11-18 20:45:38 +0800

    fourteenth

--- liao/01.py
+++ liao/01.py
@@ -22,4 +22,20 @@ print(a,type(a))
 
 n1=1.1
 n2=2.2
-print(n1=n2)
+print(n1+n2)
+
+f1=True
+f2=False
+print(f1,type(f1))
+print(f1+1)
+
+
+str1='人生苦短，我用python'
+print(str1,type(str1))
+
+name='张三'
+age=20
+
+print('我叫'+name+'今年，'+str(age)+'岁')
+
+
