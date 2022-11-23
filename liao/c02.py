90ea927fac935f0a477d0a4b96fa94355febcd66
yemtin <yemtin@qq.com>
2022-11-23 21:23:42 +0800

    twenty-second

--- liao/c02.py
+++ liao/c02.py
@@ -1,24 +1,24 @@
-  
-    
-   #多分支结构
-'''90-100  A
-   80-89   B
-   70-79   C
-   60-69   D
-   0-59    E
-'''
-score=int(input('请输入一个成绩：'))
-if score>=90 and score<=100:
-    print('A级')
-elif score>=80 and score<89:
-    print('B级')
-elif score>=70 and score<79:
-    print('C级')
-elif score>=60 and score<69:
-    print('D级')
-elif score>=0 and score<59:
-    print('E级')
+'''会员 >=200  8折
+        >=100  9折
+        不打折
+        非会员   >=200  9.5折
+          不打折'''
+answer=input('您是会员吗？y/n')
+money=float(input('请输入您的购物金额：'))
+if answer=='y':
+    if money>=200:
+        print('打9折，付款金额为：',money*0.8)
+    elif money>=100:
+        print('打八折，付款金额为：',money*0.9)
+    else:
+      print('付款金额为：',money)
+    print("会员")
 else:
-    print('成绩有误')
+     print('非会员')
+     if money>=200:
+        print('打9.5折，付款金额为：',money*0.95)
+     else:
+       print('不打折，付款金额为：',money)
 
-    
+
+     
