218d1191aa72b747b1624610846881a6aa1bb982
yemtin <yemtin@qq.com>
2022-11-25 23:50:04 +0800

    pratice_1

--- liao/c04.py
+++ liao/c04.py
@@ -1,7 +1,18 @@
-age=int(input('请输入您的年龄：'))
+#range()的三种创建方式
+'''第一种，只有一个参数(小括号中只给了一个数）'''
+r=range(10)
+print(r)
+print(list(r))
 
-if age:
-    print(age)
-else:
-    print('您的年龄为：',age)
-    
+'''第二种'''
+r=range(1,10)
+print(list(r))
+r=range(1,10,2)
+print(list(r))
+
+
+print(10 in r)
+
+
+
+
