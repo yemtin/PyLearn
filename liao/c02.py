7a2917e928680c33e44b1eaa66cda4a6c9830c42
yemtin <yemtin@qq.com>
2022-11-26 10:19:26 +0800

    twenty-fourth

--- liao/c02.py
+++ liao/c02.py
@@ -1,24 +1,28 @@
-'''会员 >=200  8折
-        >=100  9折
-        不打折
-        非会员   >=200  9.5折
-          不打折'''
-answer=input('您是会员吗？y/n')
-money=float(input('请输入您的购物金额：'))
-if answer=='y':
-    if money>=200:
-        print('打9折，付款金额为：',money*0.8)
-    elif money>=100:
-        print('打八折，付款金额为：',money*0.9)
-    else:
-      print('付款金额为：',money)
-    print("会员")
-else:
-     print('非会员')
-     if money>=200:
-        print('打9.5折，付款金额为：',money*0.95)
-     else:
-       print('不打折，付款金额为：',money)
+#for-in 循环
+for item in "python":   #将phthon里的值一次付给item
+    print(item)
 
+
+    for i  in range(10):
+        print(i)
+
+
+    for _ in range(5):
+      print("人生苦短,我用python")
+
+      #使用for循环计算1-100之间的偶数和
+      sum=0   #用于存储偶数和
+      for item in range(1,101):
+         if item%2==0:
+            sum+=item
+            print('1-100之间的偶数和为:',sum)
+
+
+            #pratice:要求输出100-999之间的水仙花数(eg:153=3*3*3+5*5*5+1*1*1)
+            for item in range(100,1000):
+               ge=item%10
+               shi=item//10%10
+               bai=item//100
+               print(ge,shi,bai)
 
      
