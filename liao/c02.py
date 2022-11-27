17019fd7463ce09c54b736e4fdf3382786b56a9a
yemtin <yemtin@qq.com>
2022-11-27 16:18:52 +0800

    break in while

--- liao/c02.py
+++ liao/c02.py
@@ -1,11 +1,10 @@
 
-'''从键盘录入密码，最多录入3次，如果正确结束循环'''
-for item in range(3):
+a=0
+while a<3:  #条件执行体
     pwd=input("请输入密码：")
-    if pwd=='8888':
-        print('密码正确')
-        break
+    if pwd=="8888":
+     print("密码正确")
+     break
     else:
-        print("密码不正确")
-
-     
+       print("密码不正确")
+       a+=1    #改变变量
