4e87041e13d3975988bcc0bace5b0e1924107782
yemtin <yemtin@qq.com>
2022-11-27 17:17:11 +0800

    rectangle

--- liao/c02.py
+++ liao/c02.py
@@ -1,14 +1,9 @@
 
-#eles
-for item in range(3):
-    pwd=input('请输入密码：')
-    if pwd=="8888":
-        print('密码正确')
-        break
-    else:
-        print('密码不正确')
-else:
-    print('对不起，三次密码均输入错误')
+'''输入一个三行四列的矩形'''
+for i in range(1,4):
+    for j in range(1,5):
+        print('*',end='\t')
+    print()
 
 
         
