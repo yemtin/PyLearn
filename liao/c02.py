a3abb0793cb9c6edef0b669b9de7868b61bb3be3
yemtin <yemtin@qq.com>
2022-11-27 16:09:16 +0800

    break in for-in

--- liao/c02.py
+++ liao/c02.py
@@ -1,10 +1,11 @@
 
-        #pratice:要求输出100-999之间的水仙花数(eg:153=3*3*3+5*5*5+1*1*1)
-for item in range(100,1000):
-    ge=item%10
-    shi=item//10%10
-    bai=item//100
-    if ge**3+shi**3+bai**3==item:
-        print(item)
+'''从键盘录入密码，最多录入3次，如果正确结束循环'''
+for item in range(3):
+    pwd=input("请输入密码：")
+    if pwd=='8888':
+        print('密码正确')
+        break
+    else:
+        print("密码不正确")
 
      
