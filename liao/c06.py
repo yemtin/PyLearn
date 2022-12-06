f74d6ae81944284b19d297b421ebff5031b8d0e7
yemtin <yemtin@qq.com>
2022-12-06 23:23:36 +0800

    change ele

--- liao/c06.py
+++ liao/c06.py
@@ -1,23 +1,10 @@
-#向列表末尾添加一个元素
-lst=[10,20,30]
-lst.append(100)
-print(lst)
-
-#extend
 lst=[10,20,30,40]
-lst2=[80,90]
-lst.extend(lst2)
+#换一个
+lst[2]=100 #索引
+print(lst)
+#换几个
+lst[1:3]=[300,400,500,600]
 print(lst)
 
 
-#insert
-lst=[19,29,39,49]
-lst.insert(1,90)
-print(lst)
 
-
-#切片
-lst3=[True,False,'hello']
-#在任意位置添加n多个元素
-lst[1:]=lst3
-print(lst)
