9871b2bcc8d0616872a7eed9f72714c061379309
yemtin <yemtin@qq.com>
2022-12-07 23:17:09 +0800

    built dict

--- liao/c06.py
+++ liao/c06.py
@@ -1,19 +1,13 @@
-#排序
-lst=[20,40,10,98,54]
-print('排序前的',lst)
-lst.sort()    #升序
-print('排序后的',lst)
-
-lst.sort(reverse=True)  #降序  reverse=False 为升序
-print(lst)
+#字典（一对一对的元素）
+#创建
+scores={'张三':100,'李四':98,'王五':45}
+print(scores)
+print(type(scores))
 
 
-print('---------------------使用内置函数sorted(),产生新的列表---------------------')
-lst=[20,40,10,98,54]
-print('原列表',lst)
-new_list=sorted(lst)
-print(lst)
-print(new_list)
+student=dict(name='jack',age=20)
+print(student)
 
 
-
+d={}
+print(d)
