c7be2e8c1a76fe43174412b0e5af937555e51b28
yemtin <yemtin@qq.com>
2022-12-08 17:43:51 +0800

    keys values items

--- liao/c05.py
+++ liao/c05.py
@@ -1,11 +1,11 @@
-#元素的获取
-#方式1
+#获取字典试图
 scores={'张三':100,'李四':98,'王五':45}
-print(scores['张三']) 
-#print(scores['陈六]) #keyerror
-  
+keys=scores.keys()
+print(keys)
+print(type(keys))
 
-#方式2
-print(scores.get('张三'))
-print(scores.get('陈六'))   #none
-print(scores.get('马奇',99))
+values=scores.values()
+print(values)
+
+items=scores.items()
+print(items)
