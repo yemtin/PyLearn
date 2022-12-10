3b506e7e651d1d62be2220fb8f3c405657ce3207
yemtin <yemtin@qq.com>
2022-12-10 23:13:22 +0800

    tuple

--- liao/c06.py
+++ liao/c06.py
@@ -1,7 +1,22 @@
-#元素的遍历
-scores={'张三':100,'李四':98,'王五':45}
-for item in scores:
-    print(item.scores[item],scores.get(item)) 
+ #元组
+   #不可变序列  字符串 元组  用小括号
+   #创建
+   #第一种
+t=('Python','world',98)
+print(t)
+print(type(t))
+  
+#第二种
+t2=tuple(('Python','world',98))
+print(t2)
+
+#空元组
+t3=tuple()
+print(t3)
+t4=()
+print(t4)
+
+
 
 
 
