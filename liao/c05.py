25f640e462e5c295044e589f561088a982725d03
yemtin <yemtin@qq.com>
2022-12-11 16:11:07 +0800

    traverse the tuple

--- liao/c05.py
+++ liao/c05.py
@@ -1,8 +1,17 @@
-#字典的特点
-d={'name':'张三','name':'李四'}
-print(d)     #key不可重复
+#元组的遍历
+t=('Python','world',98)
+#获取元组的第一种方式，使用索引
+print(t[0])
+print(t[1])
+print(t[2])
 
-d={'name':'张三','nikename':'张三'} #value
-print(d)
+
+
+#遍历元组
+for item in t:
+    print(item)
+
+
+
 
 
