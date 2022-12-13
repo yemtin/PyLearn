25c3b3a3aca3c0418fed1fea9f0e6d3ede98f4d5
yemtin <yemtin@qq.com>
2022-12-13 22:52:27 +0800

    ope of sets

--- liao/c05.py
+++ liao/c05.py
@@ -1,30 +1,11 @@
-#集合的相关操作
-s={2,3,3,4,5,5,6,7}
-#增
-s.add(10)  #增一个
-print(s)
+#集合的操作
+s1={10,20,30,40,50,60}
+s2={10,20,30,40}
+s3={10,20,30}
+#交集
+print(s2.isdisjoint(s3))  
+print(s1&s2)
 
-s.update({200,300,400})  #增几个
-print(s)
-
-#删
-s.remove(3)  #删一个  指定元素不存在 异常
-print(s)
-
-s.discard(6)  #删一个 不存在 不异常
-print(s)
-
-s.pop()    #删任意一个 不知道删哪个 不能指定
-print(s)
-
-s.clear()
-print(s)
-
-
-
-
-
-
-
-
-
+#并集
+print(s1.union(s2))
+print(s1|s2)
