04986d8503e7e078afa8d662c398652b43a98757
yemtin <yemtin@qq.com>
2022-12-14 23:54:12 +0800

    string

--- liao/c06.py
+++ liao/c06.py
@@ -1,15 +1,8 @@
-#集合之间的关系
-#子集
-s1={10,20,30,40,50,60}
-s2={10,20,30,40}
-s3={10,20,30}
-print(s2.issubset(s1))
-print(s3.issubset(s2))
-
-
-#超集
-print(s1.issuperset(s2))
-print(s2.issuperset(s3))
-
-#交集
-print(s2.isdisjoint(s3))
+#z
+# 字符串
+a='Pyhton'
+b="Python"
+c='''Python'''
+print(a,id(a))
+print(b,id(b))
+print(c,id(c))
