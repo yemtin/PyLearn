e56c37361de48c0f49ed027ebf65cdfa07f93d2d
yemtin <yemtin@qq.com>
2022-12-15 22:59:41 +0800

    search for siring

--- liao/c06.py
+++ liao/c06.py
@@ -6,3 +6,16 @@ c='''Python'''
 print(a,id(a))
 print(b,id(b))
 print(c,id(c))
+
+
+#字符串的查找
+s='hello,hello'
+print(s.index('lo'))
+print(s.rindex('lo'))
+print(s.find('lo'))
+print(s.rfind('lo'))
+print(s.index('k'))
+print(s.rindex('k'))
+print(s.find('k'))
+print(s.rfind('lo'))
+
