e8b48525fc2c6ebb2644fb3d059b9d35b1b48bd8
yemtin <yemtin@qq.com>
2022-12-18 14:54:30 +0800

    ope of sets

--- liao/c06.py
+++ liao/c06.py
@@ -1,21 +1,10 @@
-#z
-# 字符串
-a='Pyhton'
-b="Python"
-c='''Python'''
-print(a,id(a))
-print(b,id(b))
-print(c,id(c))
+s='hello,python'
+print(s.replace('python','Java'))
+s1='hello,python,python,python'
+print(s1.replace('python','java',2))  #替换前两个
 
 
-#字符串的查找
-s='hello,hello'
-print(s.index('lo'))
-print(s.rindex('lo'))
-print(s.find('lo'))
-print(s.rfind('lo'))
-print(s.index('k'))
-print(s.rindex('k'))
-print(s.find('k'))
-print(s.rfind('lo'))
+print('|'.join(s))
+print('*'.join(s))
+
 
