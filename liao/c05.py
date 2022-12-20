d244e19373300ae1b005155c8ad1b7a4ef828304
yemtin <yemtin@qq.com>
2022-12-20 23:49:28 +0800

    com of sets2

--- liao/c05.py
+++ liao/c05.py
@@ -1,39 +1,15 @@
-#字符串大小写的转换
 s='hello,python'
-a=s.upper()  #转大
-print(a)
-b=s.lower() #转小
-print(b)
-c=s.swapcase() #小->大 大->小
-print(c)
-d=s.capitalize() #all the first
-print(d)
-f=s.title()  #each first 
-print(f)
+print(s.replace('python','Java'))
+s1='hello,python,python,python'
+print(s1.replace('python','java',2))  #替换前两个
 
 
-
-#对齐
-s="hello,Python"
-print(s.center(20,'*'))  #居中对齐
-
-print(s.ljust(20,"*"))  #左对齐
-print(s.ljust(10))
-print(s.ljust(20)) 
-
-print(s.rjust(20,'*'))  #右对齐
-
-print(s.zfill(20))  #zero fill
-print(s.zfill(10))  #小于 返回元字符
-print('-8910'.zfill(8))
+print('|'.join(s))
+print('*'.join(s))
 
 
-#劈分
-s='hello world Python'
-print(s.split())
-lst=s.split()
-print(lst)
-s1='hello|world|Python'
-print(s1.split(sep='|'))  #sep指定劈分符
-print(s1.split(sep='|',maxsplit=1))  #规定最大劈分
-
+print('apple'>'app')
+print('apple'>'banana')  #比较原始值
+print(ord('a'),ord('b'))  #得到原始值
+print(chr(97),chr(98))    #得到原来的字符
+print(ord('p'),ord('a'))
