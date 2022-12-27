1104f228b6bff5075142e5832d9cb2153ad96a02
yemtin <yemtin@qq.com>
2022-12-27 20:05:56 +0800

    def of faction

--- liao/c06.py
+++ liao/c06.py
@@ -1,23 +1,9 @@
-#格式化
-#%
-name='张三'
-age=20
-print('我叫%s,今年%d岁' % (name,age))
+def calc(a,b):  #a,b称为形势参数，简称形参
+    c=a+b
+    return(c)
 
+result=calc(10,20)  #10，20称为实参
+print(result)
 
-#{}
-print('我叫{0},今年{1}岁'.format(name,age))
-
-print(f'我叫{name},今年{age}')
-
-
-#精度宽度
-print('%10d' % 99) #10表示总宽度
-print('%.3f' % 3.1415926)  #3表示小数点后几位
-print('%10.3f' % 3.1415926) 
-
-print('{0:.3}' . format(3.1415926))  #一共三位
-print('{:.3f}' . format(3.1415926))  #三位小数
-
-
-
+res=calc(b=10,a=20)
+print(res)
