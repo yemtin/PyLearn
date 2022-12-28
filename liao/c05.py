3e78a72c60d89d02b3678da2bf8985964ea29e67
yemtin <yemtin@qq.com>
2022-12-28 23:59:17 +0800

    def of fac

--- liao/c05.py
+++ liao/c05.py
@@ -1,12 +1,12 @@
-#编码和解码
-s='天涯共此时'
-#编码
-print(s.encode(encoding='GBK'))  #在GBK这种编码格中一个中文占两个字节
-print(s.encode(encoding='UTF-8'))  #占三格
+def fun(num):
+    odd=[]
+    even=[]
+    for i in num:
+        if i%2:
+            odd.append(i)
+        else:
+            even.append(i)
+    return odd,even
 
-bytes=s.encode(encoding='GBK')  #编码
-print(bytes.decode(encoding='GBK')) #解码
-bytes=s.encode(encoding='UTF-8')
-print(bytes.decode(encoding='UTF-8'))
-
-
+lst=[10,20,29,34,23,44,53,55]
+print(fun(lst))
