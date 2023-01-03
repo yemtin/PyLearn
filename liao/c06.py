ef7a8021f37ae746bdb15d5f8196b32c7d238383
yemtin <yemtin@qq.com>
2023-01-03 23:16:47 +0800

    class2

--- liao/c06.py
+++ liao/c06.py
@@ -20,3 +20,26 @@ class Student:   #类的名称 由一个单词或多个单词 首字母大写
 #在类之外叫函数
 def drink():
     print('喝水')
+
+#创建student类对象
+stu1=Student('张三',20)
+print(id(stu1))
+print(type(stu1))
+print(stu1)
+print('-------------------------------------------')
+print(id(Student))
+print(type(Student))
+print(Student)
+print('-----------------------------------------------------')
+stu1.eat()
+print(stu1.age)
+print(stu1.name)
+print('--------------------------------')
+Student.eat(stu1)
+
+#类属性的使用方法
+print(Student.native_pace)
+stu1=Student('张三',20)
+stu2=Student('李四',30)
+print(stu1.native_pace)
+print(stu2.native_pace)
