b1c283e687d1c7803fe2b6b5fdbc6b5f3909d4bf
yemtin <yemtin@qq.com>
2023-01-05 17:55:22 +0800

    dy binding

--- liao/c05.py
+++ liao/c05.py
@@ -1,22 +1,28 @@
-#递归函数
-def fac(n):
-    if n==1:
-        return 1
-    else:
-        return n*fac(n-1)
-print(fac(6))
-print(fac(1))
+#动态绑定属性
+#定义函数
+class Student:
+    def __init__(self,name,age):  #局部变量
+        self.name=name            #实例变量
+        self.age=age
+    def eat(self):
+        print(self.name+'在吃饭')
+
+#调用函数
+stu1=Student('张三',20)
+stu2=Student('李四',30)
+print(id(stu1))
+print(id(stu2))
+stu1.gender='女'
+print(stu1.name,stu1.age,stu1.gender)
+print(stu2.name,stu2.age)
 
 
-def fib(n):
-    if n==1:
-        return 1
-    elif n==2:
-        return 1
-    else:
-        return fib(n-1)+fib(n-2)
-print(fib(4))
+stu1.eat()
+stu2.eat()
 
-
-
-
+#动态方法
+def show():
+    print('定义在类之外得')
+stu1.show=show
+stu1.show()
+ 
