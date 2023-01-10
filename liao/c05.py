b20690bab12bc3ced8b09864ccf9f9e459b30682
yemtin <yemtin@qq.com>
2023-01-10 17:31:16 +0800

    object

--- liao/c05.py
+++ liao/c05.py
@@ -10,6 +10,8 @@ class Student(Person):
     def __init__(self, name, age,stu_no):
         super().__init__(name, age)
         self.stu_no=stu_no
+    def info(self): 
+        print(self.stu_no)  
 
 class Teacher(Person):
     def __init__(self, name, age,teachofyear):
@@ -19,5 +21,6 @@ class Teacher(Person):
 stu=Student('张三',20,'1001')
 teacher=Teacher('李四',34,10)
 
-#stu.info()Teacher.info()
-print(Student.info)
+stu.info()
+Teacher.info()
+
--- liao/c06.py
+++ liao/c06.py
@@ -1,23 +1,12 @@
- #封装
-class Car:
-    def __init__(self,brand):
-        self.brand=brand
-    def start(self):
-        print('汽车已启动')
-car=Car('宝马')
-car.start()
-print(car.brand)
-
-
-class Student:
+class Studend:
+    #pass
     def __init__(self,name,age):
         self.name=name
-        self.__age=age  #年龄不希望在类外部被使用，加__
-    def show(self):
-        print(self.name,self.__age)
-stu=Student('张三',20)
-stu.show()
-#在类外面使用age
-#print(stu.__age)  #错误
-#print(dir(stu))  #查询怎么使用
-print(stu._Student__age)
+        self.age=age
+    def __str__(self):
+        return'我得名字是{0}，今年{1}岁'.format(self.name,self.age)
+stu=Studend('张三',20)
+print(dir(stu))
+print(stu)
+print(type(stu))
+
