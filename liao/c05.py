ed21a22f8fd3a5bae35326543bef06621e160a9c
yemtin <yemtin@qq.com>
2023-01-10 18:08:23 +0800

    ...

--- liao/c05.py
+++ liao/c05.py
@@ -1,26 +1,21 @@
-#继承
-class Person(object):
-    def __init__(self,name,age):
-        self.name=name
-        self.age=age
-    def info(self):
-        print(self.name,self.age)
+class Animal(object):
+    def eat(self):
+        print('动物会吃')
+class Dog(Animal):
+    def eat(self):
+        print('狗吃骨头...')
+class Cat(Animal):
+    def eat(self):
+       print('猫吃鱼...')
 
-class Student(Person):
-    def __init__(self, name, age,stu_no):
-        super().__init__(name, age)
-        self.stu_no=stu_no
-    def info(self): 
-        print(self.stu_no)  
-
-class Teacher(Person):
-    def __init__(self, name, age,teachofyear):
-        super().__init__(name, age)
-        self.teachofyear=teachofyear
-
-stu=Student('张三',20,'1001')
-teacher=Teacher('李四',34,10)
-
-stu.info()
-Teacher.info()
+class Person:
+    def eat(self):
+        print('人吃五谷杂粮')
+#定义函数         
+def fun(obj):
+    obj.eat()
+#调用函数
+fun(Cat())
+fun(Dog())
+fun(Animal)
 
