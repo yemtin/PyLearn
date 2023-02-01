1c15dffd730263977595e4aa6a245a3ad405f82a
yemtin <yemtin@qq.com>
2023-02-01 21:48:34 +0800

    module

---
+++ liao/c08.py
@@ -0,0 +1,12 @@
+import math   #关于数学运算
+print(id(math))
+print(type(math))
+print(math)
+print(math.pi)
+
+print('--------------------------------------')
+print(dir(math))
+
+from math import pi
+print(pi)
+
---
+++ liao/c09.py
@@ -0,0 +1,5 @@
+#c09.py中导入calc自定义模块使用
+import calc
+print(calc.add(10,20))
+from calc import add
+print(10,20)
