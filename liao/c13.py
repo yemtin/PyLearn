d2d5ddf72e2f4cedebd8b2264db55410c431236c
yemtin <yemtin@qq.com>
2023-08-06 15:32:53 +0800

    pratice12

--- liao/c13.py
+++ liao/c13.py
@@ -1,26 +1,16 @@
-class Instrument():
-   def make_sound(self):
-    pass
-
-class Erhu(Instrument):
-    def make_sound(self):
-       print('erhu is playing')
-class Violin(Instrument):
-    def make_sound(self):
-        print('violin is playing')
-class Piano(Instrument):
-    def make_sound(self):
-        print('piano is playing')
-
-def play(instrument):
-    Instrument.make_sound()
-    
-    if __name__=='_main_':
-     play(Erhu())
-     play(Violin())
-     play(Piano())
-
-
-
-
-
+def getText():
+    txt=open("D:\code\PyLearn\liao\哈利波特1-7英文原版.txt",'r').read()
+    txt.lower()
+    for ch in "!@#$%""^&*(){}_+:<>?~'',.":
+        txt=txt.replace(ch,' ')
+    return txt
+harrypoterTxt=getText()
+words=harrypoterTxt.split()
+counts={}
+for word in words:
+    counts[word]=counts.get(word,0)+1
+items=list(counts.items())
+items.sort(key=lambda x:x[1],reverse=True)
+for i in range(10000):
+    word,count=items[i]
+    print('{0:<10}{1:>5}'.format(word,count))
